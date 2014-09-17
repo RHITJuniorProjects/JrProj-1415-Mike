838197077cc7cd9be74b703d4f2d3dccd9da217a
Mason Schneider <mason@Masons-Mac.local>
2014-09-16 18:15:10 -0700

    Mason Schneider and Tyler Duffy feature 3 iOS

--- Code/iOS/TasksTableViewController.m
+++ Code/iOS/TasksTableViewController.m
@@ -10,9 +10,12 @@
 
 @interface TasksTableViewController ()
 
+@property NSMutableArray *tasks;
+
 @end
 
 @implementation TasksTableViewController
+
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -27,11 +30,9 @@
 {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tasks = [[NSMutableArray alloc] init];
+    [self.tasks addObject:@"Task 1"];
+    [self.tasks addObject:@"Task 2"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,28 +45,26 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return self.tasks.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TaskCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = [self.tasks objectAtIndex:indexPath.row];
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
