fedd6c20c3668e6f3463d0b78da9837505c55f2d
Carter Grove <grovecj@rose-hulman.edu>
2014-09-23 15:18:44 -0400

    Firebase App Skeleton

---
+++ Code/iOS/HenryTests/HenryTests.m
@@ -0,0 +1,40 @@
+//
+//  HenryTests.m
+//  HenryTests
+//
+//  Created by Carter Grove on 9/21/14.
+//  Copyright (c) 2014 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HenryTests : XCTestCase
+
+@end
+
+@implementation HenryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
