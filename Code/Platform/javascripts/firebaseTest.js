427f4ecbb9a86a6cd86aff477c336556aab49cab
Matt Rocco <roccoma@rose-hulman.edu>
2014-09-30 10:43:00 -0400

    Added proof of concept code for retrieving user's project teams andprojects from firebase

--- Code/Platform/javascripts/firebaseTest.js
+++ Code/Platform/javascripts/firebaseTest.js
@@ -1,13 +1,24 @@
 window.onload = function(){
 var fb = new Firebase('https://henry371.firebaseio.com');
-document.getElementById("setValue").addEventListener('click',function(event){
-var newVal = document.getElementById("firebaseValue").value
-fb.set({test:newVal})
-})
-//LISTEN FOR REALTIME CHANGES
-fb.on("value", function(data) {
-  var received = data.val() ? data.val().test : "";
-  document.getElementById("testValue").innerHTML = document.getElementById("testValue").innerHTML + "<br/>" + received
 
-});
+document.getElementById("setValue").addEventListener('click',function(event){
+var userName = document.getElementById("fireBaseUserName").value
+
+fbUserName = fb.child("user/" + userName +"/teams");
+var teams;
+fbUserName.once("value", function(data) {
+  var received = data.val() ? data : "";
+  received.forEach( function(team){
+  		alert(team.name() + " " + team.val());
+  		teams = teams + " " + team.val();
+  	}
+  	alert(teams)	
+  )})
+
 }
+);
+}
+//LISTEN FOR REALTIME CHANGES
+
+
+ 
