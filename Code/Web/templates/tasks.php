fa45d93ccf62fcd1ddeec3941e3c73f253ab4634
Mark Hein <heinmr@rose-hulman.edu>
2014-10-02 17:33:32 -0400

    Actually Made Task Page, set database to personal
    Mark Hein (heinmr), David Robinson (robinsdm)

---
+++ Code/Web/templates/tasks.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>Henry - Teams</title>
+	<?php require 'header.php';?>
+</head>
+<body>
+	<?php require 'topbar.php';?>
+	<div class="row text-center">
+		<h1>My Tasks</h1>
+	</div>
+	<table style="width:100%">
+		<tr>
+			<td>Task Name</td>
+			<td>Assigned User</td> 
+			<td>Points</td>
+		</tr>
+		<tr>
+			<td>Change html pages</td>
+			<td>Jeff</td> 
+			<td>3</td>
+		</tr>
+		<tr>
+			<td>Update database</td>
+			<td>Fred</td> 
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>Delete unused code</td>
+			<td>Claire</td> 
+			<td>2</td>
+		</tr>
+	</table>
+</body>
+</html>
