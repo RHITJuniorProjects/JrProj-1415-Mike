a92ad4744ba008bbf5c34d4e7cfe7b08e4bc5aec
Duffy <duffytj@klondike.csse.rose-hulman.edu>
2014-10-05 16:30:08 -0400

    made a story board for sprint 2 ios teams

---
+++ Code/iOS/Henry/ProjectsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectsTableViewController.h
+//  Henry
+//
+//  Created by Duffy, Tyler J on 10/5/14.
+//  Copyright (c) 2014 Rose-Hulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectsTableViewController : UITableViewController
+
+@end
