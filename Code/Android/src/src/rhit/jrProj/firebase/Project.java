abc206e1a7e390db5422d067560c2427c6ae9f40
Austin Willis <willisaj@rose-hulman.edu>
2014-10-04 22:21:35 -0400

    Showing list of Projects (using hard-coded Projects (not pulling from Firebase))
    Austin Willis - willisaj
    Edward Goldthorpe - goldthea

--- Code/Android/src/src/rhit/jrProj/firebase/Project.java
+++ Code/Android/src/src/rhit/jrProj/firebase/Project.java
@@ -1,5 +1,36 @@
 package rhit.jrProj.firebase;
 
+import java.util.ArrayList;
+
 public class Project {
 
+	/**
+	 * A List of tasks that are contained within the Milestone
+	 */
+	private ArrayList<Milestone> milestones = new ArrayList<Milestone>();
+
+	/**
+	 * The milestone Number within the project
+	 */
+	private int projectNumber;
+
+	/**
+	 * Creates a Milestone object
+	 * 
+	 * @param tasks
+	 * @param number
+	 */
+	public Project(ArrayList<Milestone> milestones, int number) {
+		this.milestones = milestones;
+		this.projectNumber = number;
+	}
+
+	public int getMilestoneNumber() {
+		return this.projectNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Project " + this.projectNumber;
+	}
 }
--- Code/Android/src/src/rhit/jrProj/henry/dummy/DummyContent.java
+++ Code/Android/src/src/rhit/jrProj/henry/dummy/DummyContent.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import rhit.jrProj.firebase.Milestone;
+import rhit.jrProj.firebase.Project;
 import rhit.jrProj.firebase.Task;
 
 /**
@@ -20,30 +21,55 @@ public class DummyContent {
 	 * An array of sample (dummy) items.
 	 */
 	public static List<Milestone> ITEMS = new ArrayList<Milestone>();
+	
+	public static List<Project> PROJECT_ITEMS = new ArrayList<Project>();
 
 	/**
 	 * A map of milestone (dummy) items, by ID.
 	 */
 	public static Map<String, Milestone> ITEM_MAP = new HashMap<String, Milestone>();
+	
+	public static Map<String, Project> PROJECT_MAP = new HashMap<String, Project>();
 
 	static {
+		ArrayList<Milestone> milestones = new ArrayList<Milestone>();
+		
 		// Add 3 sample items.
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		tasks.add(new Task());
 		tasks.add(new Task());
-		addItem(new Milestone(tasks, 1));
+		milestones.add(new Milestone(tasks, 1));
 		tasks = new ArrayList<Task>();
 		tasks.add(new Task());
-		addItem(new Milestone(tasks, 2));
+		milestones.add(new Milestone(tasks, 2));
 		tasks = new ArrayList<Task>();
 		tasks.add(new Task());
 		tasks.add(new Task());
 		tasks.add(new Task());
-		addItem(new Milestone(tasks, 3));
+		milestones.add(new Milestone(tasks, 3));
+		
+		for (Milestone milestone : milestones) {
+			addItem(milestone);
+		}
+		
+		//  Add 3 sample Projects
+		ArrayList<Project> projects = new ArrayList<Project>();
+		projects.add(new Project(milestones, 1));
+		projects.add(new Project(new ArrayList<Milestone>(), 2));
+		projects.add(new Project(new ArrayList<Milestone>(), 3));
+		
+		for (Project project : projects) {
+			addItem(project);
+		}
 	}
 
 	private static void addItem(Milestone item) {
 		ITEMS.add(item);
 		ITEM_MAP.put(item.toString(), item);
+	}
+	
+	private static void addItem(Project item) {
+		PROJECT_ITEMS.add(item);
+		PROJECT_MAP.put(item.toString(), item);
 	}
 }
