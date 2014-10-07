a3b607737864b079894f6cf8644d6097794a48e5
Zach Hull <hullzr@rose-hulman.edu>
2014-10-07 17:38:27 -0400

    Zach Hull & Donnie Waters test scripts
    Web/android feature 2 test scripts

---
+++ Code/QA/AndroidFeature2Script/SikuliScripts.sikuli/SikuliScripts.py
@@ -0,0 +1,32 @@
+if exists("1412713977063.png"):
+    print "Project 1 displaying correctly."
+else:
+    print "ERROR: Project 1 not displaying."
+
+click("1412714075748.png")
+if exists("1412714147924.png"):
+    print "Milestones displaying correctly."
+else:
+    print "ERROR: Milestones not displaying correctly."
+click("1412715048927.png")
+if exists("1412714470529.png"):
+    print "Milestone 1 page displaying correctly."
+else:
+    print "ERROR: Milestone 1 page not displaying correctly."
+click("1412714557955.png")
+click("1412714591284.png")
+click("1412714607483.png")
+click("1412714624105.png")
+if exists("1412714701632.png"):
+    print "Milestone 2 page displaying correctly."
+else:
+    print "ERROR: Milestone 2 page not displaying correctly."
+click("1412714731914.png")
+if exists("1412714822090.png"):
+    print "Tasks displaying correctly."
+else:
+    print "ERROR: Tasks not displaying correctly."
+click("1412715541620.png")
+click("1412715584036.png")
+click("1412715606642.png")
+print "Back to start"
