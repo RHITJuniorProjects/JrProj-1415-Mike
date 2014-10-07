76b3c0ac251b26712bc395345ade6b977843c5c3
Zach Hull <hullzr@rose-hulman.edu>
2014-10-07 16:28:46 -0400

    QA Web Testing Script

---
+++ Code/QA/WebScripts.sikuli/WebScripts.py
@@ -0,0 +1,58 @@
+##Start Milestone 1 Test
+reg1 = Region(1138,791,263,155)
+reg1.find("1412712151316.png")
+reg1.click("1412712175103.png")
+if exists("1412711717706.png"):
+    print("Milestone1 worked bro")
+else:
+    print("Milestone1 didnt work bro")
+    
+click("1412712293265.png")
+
+##Start Milestone 2 Test
+
+reg2 = Region(1513,796,239,128)
+reg2.find("1412712430431.png")
+reg2.click("1412710856163.png")
+if exists("1412711717706.png"):
+    print("Milestone2 worked bro")
+else:
+    print("Milestone2 didnt work bro")
+
+click("1412712293265.png")
+
+##Start Milestone 3 Test
+reg3 = Region(1119,546,283,157)
+reg3.find("1412712544641.png")
+reg3.click("1412712572730.png")
+if exists("1412711717706.png"):
+    print("Milestone3 worked bro")
+else:
+    print("Milestone3 didnt work bro")
+
+click("1412712293265.png")
+
+
+##Start Milestone 4 Test
+reg4 = Region(1108,256,316,249)
+reg4.find("1412710816759.png")
+reg4.click("1412710856163.png")
+if exists("1412711717706.png"):
+    print("Milestone4 worked bro")
+else:
+    print("Milestone4 didnt work bro")
+
+click("1412712293265.png")
+
+##Start Milestone 5 Test
+reg5 = Region(1503,305,271,137)
+reg5.find("1412712636738.png")
+reg5.click("1412712648136.png")
+if exists("1412711717706.png"):
+    print("Milestone5 worked bro")
+else:
+    print("Milestone5 didnt work bro")
+
+click("1412712293265.png")
+
+##End Tests
