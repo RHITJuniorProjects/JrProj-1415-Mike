449c9171471eb17f86e6b28b78c4bbf20a9f8014
hansondg <danielghanson93@gmail.com>
2014-10-08 11:33:02 -0400

    fixed getting name from firebase

--- Code/Web/public/milestones.js
+++ Code/Web/public/milestones.js
@@ -5,8 +5,7 @@ $(function(){
 	//var projectId = $.cookie('project');
 	var projectId = '-JYcg488tAYS5rJJT4Kh';
 	var project = projects.get(projectId);
-	var project.getName(function(name){
-		console.log(name);
+	project.getName(function(name){
 		$title.html(name);
 	});
 });
