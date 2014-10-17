ad240599fb4fa7096445c8661ceb494e9f608953
hansondg <danielghanson93@gmail.com>
2014-10-17 15:40:11 -0400

    added itemsadded/itemsremoved

--- Code/Web/public/henry.js
+++ Code/Web/public/henry.js
@@ -1,10 +1,12 @@
 
 /* this file contains classes and utility functions that are used everywhere on the website */
-var firebase = new Firebase("https://henry371.firebaseIO.com");
+var firebase = new Firebase("https://henry-production.firebaseIO.com");
 
 // table object manages a table of values in the database, use get to get objects from the database
 // by uid
 function Table(factory,firebase){
+	this.__onChildAdded = null;
+	this.__onChildRemoved = null;
 	this.__factory = factory;
 	this.__firebase = firebase;
 }
@@ -13,9 +15,46 @@ Table.prototype = {
 	get:function(uid){
 		return this.__factory(this.__firebase.child(uid));
 	},
-	add:function(obj){
-
+	onItemAdded:function(callback){
+		if(!this.__onChildAdded){
+			this.__onChildRemoved = [];
+			this.__firebase.on('child_added',function(snap){
+				var val = this.__factory(snap.val());
+				this.__onChildAdded.forEach(function(callback){
+					callback(val);
+				});
+			});
+		}
+		this.__onChildAdded.push(callback);
+	},
+	onItemRemoved:function(callback){
+		if(!this.__onChildRemoved){
+			this.__onChildRemoved = [];
+			this.__firebase.on('child_removed',function(snap){
+				var val = this.__factory(snap.val());
+				this.__onChildRemoved.forEach(function(callback){
+					callback(val);
+				});
+			});
+		}
+		this.__onChildRemoved.push(callback);
 	}
+};
+
+var users = new Table(function(fb){return new User(fb);},firebase.child('users'));
+
+function User(firebase){
+	this.firebase = firebase;
+	this.uid = firebase.name();
+	this.__name = firebase.child('name');
+	this.__email = firebase.chile('email');
+}
+
+User.prototype = {
+	getName:function(callback){
+		this.__name
+	}
+
 };
 
 function Project(firebase){
