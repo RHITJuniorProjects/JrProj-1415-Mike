1b3a566e2b9f3f0fc6f13bf8c82f3972d7ad6d73
lamberce <lamberce@rose-hulman.edu>
2014-10-19 14:36:06 -0400

    Chart
    Chart for feature

---
+++ Code/Web/public/home-test.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column',
+            margin: 75,
+            options3d: {
+                enabled: true,
+                alpha: 10,
+                beta: 25,
+                depth: 70
+            }
+        },
+        title: {
+            text: '3D chart with null values'
+        },
+        subtitle: {
+            text: 'Notice the difference between a 0 value and a null point'
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        xAxis: {
+            categories: Highcharts.getOptions().lang.shortMonths
+        },
+        yAxis: {
+            opposite: true
+        },
+        series: [{
+            name: 'Projects',
+            data: [2, 3, null, 4, 0, 5, 1, 4, 6, 3]
+        }],
+		colors: ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', 
+   '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a']
+    });
+});
