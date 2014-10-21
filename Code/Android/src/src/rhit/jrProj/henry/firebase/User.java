70f7cfa6467de5b6fd743cabe2a6a3f49f653ee0
Austin Willis <willisaj@rose-hulman.edu>
2014-10-20 22:34:22 -0400

    Fixing breaks in master
    Austin Willis - willisaj

--- Code/Android/src/src/rhit/jrProj/henry/firebase/User.java
+++ Code/Android/src/src/rhit/jrProj/henry/firebase/User.java
@@ -1,39 +1,3 @@
-
-package rhit.jrProj.henry.firebase;
-
-import com.firebase.client.ChildEventListener;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.FirebaseError;
-
-public class User implements ChildEventListener{
-
-	public void onCancelled(FirebaseError arg0) {
-		// TODO Auto-generated method stub.
-		
-	}
-
-	public void onChildAdded(DataSnapshot arg0, String arg1) {
-		// TODO Auto-generated method stub.
-		
-	}
-
-	public void onChildChanged(DataSnapshot arg0, String arg1) {
-		// TODO Auto-generated method stub.
-		
-	}
-
-	public void onChildMoved(DataSnapshot arg0, String arg1) {
-		// TODO Auto-generated method stub.
-		
-	}
-
-	public void onChildRemoved(DataSnapshot arg0) {
-		// TODO Auto-generated method stub.
-		
-	}
-	//TODO
-}
-=======
 package rhit.jrProj.henry.firebase;
 
 import java.util.ArrayList;
