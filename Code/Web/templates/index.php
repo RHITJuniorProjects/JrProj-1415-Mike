65d0c5755afd476c1b5a2749cdf568c3fb709246
Mark Hein <heinmr@rose-hulman.edu>
2014-10-31 11:31:03 -0400

    Added pretty GUI for homepage
    Mark Hein

--- Code/Web/templates/index.php
+++ Code/Web/templates/index.php
@@ -1,15 +1,31 @@
 <html>
 	<head>
-		<title>Henry</title>
+		<title>Henry - Homepage</title>
 		<?php require 'header.php';?>
 	</head>
-	<body>
+	<body class="wide">
 		<?php require 'topbar.php';?>
-		<div>
-			<h1>Henry</h1>
-			<a href="/login">Login</a>
-			<a href="/register">Register</a>
-			<a href="/projects">Projects</a>
+		<div class="columns main-content">
+			<ul class="example-orbit" data-orbit>
+			  <li>
+			    <img src="i3.jpg" alt="slide 1" />
+			    <div class="orbit-caption">
+			      Caption One.
+			    </div>
+			  </li>
+			  <li class="active">
+			    <img src="i2.jpg" alt="slide 2" />
+			    <div class="orbit-caption">
+			      Caption Two.
+			    </div>
+			  </li>
+			  <li>
+			    <img src="i1.jpg" alt="slide 3" />
+			    <div class="orbit-caption">
+			      Caption Three.
+			    </div>
+			  </li>
+			</ul>
 		</div>
 	</body>
 </html>
