663595e974de6ec7111ea1f065be1526b03b437a
rockwotj <rockwotj@rose-hulman.edu>
2014-11-01 23:45:20 -0400

    Finished MapTests

--- Code/Android/test/src/MapTest.java
+++ Code/Android/test/src/MapTest.java
@@ -1,11 +1,10 @@
 import junit.framework.TestCase;
 import rhit.jrProj.henry.firebase.Map;
 
-
 public class MapTest extends TestCase {
 
 	private Map<String, String> map;
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -14,26 +13,38 @@ public class MapTest extends TestCase {
 	}
 
 	public void testGetValue() {
+
+		assertEquals("5", this.map.getValue("Five"));
+		try {
+			this.map.getValue("Ten");
+			assertTrue(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	public void testReplaceValue() {
+		assertTrue(this.map.replaceValue("Five", "5+9"));
+		assertFalse(this.map.getAllValues().contains("5"));
+		assertTrue(this.map.getAllValues().contains("5+9"));
+	}
+
+	public void testPut() {
 		assertTrue(this.map.put("Four", "4"));
 		assertFalse(this.map.put("Five", "5+9"));
 		assertTrue(this.map.getAllKeys().contains("Four"));
 		assertFalse(this.map.getAllValues().contains("5+9"));
-	}
-
-	public void testReplaceValue() {
-		fail("Not yet implemented");
-	}
-
-	public void testPut() {
-		fail("Not yet implemented");
+		assertEquals(2, this.map.size());
 	}
 
 	public void testRemove() {
-		fail("Not yet implemented");
+		assertTrue(this.map.remove("Five"));
+		assertEquals(0, this.map.size());
+		assertFalse(this.map.remove("Ten"));
 	}
 
 	public void testSize() {
-		fail("Not yet implemented");
+		assertEquals(1, this.map.size());
 	}
 
 }
