94700860a13ccfdd81236c6d708554179a42ec24
Carter Grove <grovecj@rose-hulman.edu>
2014-11-04 14:15:11 -0500

    Form validation for current estimate field on iPad. iPad keyboard now hides when the background view is tapped

--- Code/iOS/Henry/Henry/HenryTaskDetailViewController.m
+++ Code/iOS/Henry/Henry/HenryTaskDetailViewController.m
@@ -45,10 +45,25 @@
 }
 
 - (IBAction)updateCurrentTimeEstimate:(id)sender {
-    NSDictionary *newValue = @{@"updated_time_estimate":[NSNumber numberWithDouble:[self.currentEstimateField.text doubleValue]]};
-    self.fb = [self.fb childByAppendingPath:[NSString stringWithFormat:@"projects/%@/milestones/%@/tasks/%@", self.ProjectID, self.MileStoneID, self.taskID]];
-    [self.fb updateChildValues:newValue];
-    self.fb = [HenryFirebase getFirebaseObject];
+    bool status;
+    NSScanner *scanner;
+    double result;
+    
+    scanner = [NSScanner scannerWithString:self.currentEstimateField.text];
+    status = [scanner scanDouble:&result];
+    status = status && scanner.scanLocation == self.currentEstimateField.text.length;
+    
+    if (status) {
+        NSDictionary *newValue = @{@"updated_time_estimate":[NSNumber numberWithDouble:[self.currentEstimateField.text doubleValue]]};
+        self.fb = [self.fb childByAppendingPath:[NSString stringWithFormat:@"projects/%@/milestones/%@/tasks/%@", self.ProjectID, self.MileStoneID, self.taskID]];
+        [self.fb updateChildValues:newValue];
+        self.fb = [HenryFirebase getFirebaseObject];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Estimated hours must be a number" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+        return;
+    }
+
 }
 
 -(void)updateInfo:(FDataSnapshot *)snapshot {
