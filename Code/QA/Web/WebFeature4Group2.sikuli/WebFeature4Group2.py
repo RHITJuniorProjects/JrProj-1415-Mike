2c2402b926586183263cd408308c0bb4fffc807e
Zach Hull <hullzr@rose-hulman.edu>
2014-11-06 11:39:43 -0500

    Lines of Code update for web script

--- Code/QA/Web/WebFeature4Group2.sikuli/WebFeature4Group2.py
+++ Code/QA/Web/WebFeature4Group2.sikuli/WebFeature4Group2.py
@@ -17,6 +17,19 @@ click("1415127597657.png")
 if exists("1415127628828.png"):
     find("1415127664601.png")
     print("Due date found for Mahout project")
-    print("Due dates found, test complete.")
 else:
     print("Mahout project due date not found")
+
+##Testing the ability to check lines of code for a particular project
+click("1415284303200.png")
+find("1415284340223.png")
+click("1415284366546.png")
+find("1415284391261.png")
+click("1415284400127.png")
+if exists("1415284563761.png"):
+    find("1415284563761.png")
+    print("Lines of code found")
+    print("Test Complete.")
+else:
+    print("Lines of code not found")
+
