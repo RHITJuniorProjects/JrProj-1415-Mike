7e3cbdf45038ad20e4b4903eee9da636db88128a
Deebow09 <watersdr@rose-hulman.edu>
2014-11-06 23:02:46 -0500

    Finished Android and iOS scripts for S4F2

--- Code/QA/iOS/iOSLoginScript.sikuli/iOSLoginScript.py
+++ Code/QA/iOS/iOSLoginScript.sikuli/iOSLoginScript.py
@@ -1,8 +1,8 @@
 #valid login check
 click("1414005987057.png")
-type("test1@test.com" + Key.TAB + "test")
+type("test2@test.com" + Key.TAB + "test")
 click("1414001448736.png")
-sleep(10)
+wait(10)
 if exists("1414001491047.png"):
     print("Login Successful")
 else:
@@ -10,22 +10,22 @@ else:
 
 #logout check
 click("1414001575740.png")
-sleep(3)
+wait(3)
 click("1414001657851.png")
-sleep(10)
+wait(10)
 if exists("1414001750444.png"):
     print("Logout successful")
 else:
     print("ERROR: Logout unsuccessful")
 
 #saving credentials check
-if exists("1414001792915.png"):
+if exists("1415321345982.png"):
     print("Credentials correctly saved")
 else:
     print("ERROR: Credentials not saved")
 
 #invalid login check
-click("1414001973975.png")
+click("1415321345982.png")
 type(Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE + 
         Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE +
         Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE +
@@ -33,8 +33,8 @@ type(Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE + Key.BACKSPACE +
 #14 backspaces...
 type("foo@foo.com" + Key.TAB + "foo1")
 click("1414002769171.png")
-sleep(5)
-if exists("1414002796001.png"):
+wait(5)
+if exists("1415321455842.png"):
     print("Invalid user check successful")
 else:
     print("ERROR: Invalid user check unsuccessful")
