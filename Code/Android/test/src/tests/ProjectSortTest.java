4d06adaf310bfc598c6144776fa13e88d841cb27
Skornage <millerna@rose-hulman.edu>
2014-11-09 15:26:34 -0500

    Updated test cases to include reverse alphabetical, upper and lower case, and numbers.

--- Code/Android/test/src/tests/ProjectSortTest.java
+++ Code/Android/test/src/tests/ProjectSortTest.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import android.util.Log;
+import rhit.jrProj.henry.firebase.Milestone;
+import rhit.jrProj.henry.firebase.Project;
+import rhit.jrProj.henry.firebase.Task;
 import junit.framework.TestCase;
 
 public class ProjectSortTest extends TestCase {
 
 	private ArrayList<String> projects;
-	private Comparator<String> comp;
+	private Comparator<String> compAZ;
+	private Comparator<String> compZA;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -19,23 +24,31 @@ public class ProjectSortTest extends TestCase {
 		this.projects.add("Project B");
 		this.projects.add("Android Project");
 		this.projects.add("IOS Project");
-		this.comp = new Comparator<String>() {
-
+		
+		this.compAZ = new Comparator<String>() {
+			@Override
 			public int compare(String lhs, String rhs) {
-
 				if (lhs instanceof String && rhs instanceof String) {
 					return ((String) lhs).compareToIgnoreCase((String) rhs);
 				}
 				return 0;
-
 			}
-
 		};
-
+		
+		this.compZA = new Comparator<String>() {
+			@Override
+			public int compare(String lhs, String rhs) {
+				if (lhs instanceof String && rhs instanceof String) {
+					return -1
+							* ((String) lhs).compareToIgnoreCase((String) rhs);
+				}
+				return 0;
+			}
+		};
 	}
 
 	public void testSortSmall() {
-		Collections.sort(projects, comp);
+		Collections.sort(projects, compAZ);
 		ArrayList<String> answer = new ArrayList<String>();
 		answer.add("ABC");
 		answer.add("Android Project");
@@ -68,8 +81,6 @@ public class ProjectSortTest extends TestCase {
 		answer.add("Swift");
 		answer.add("Tablet");
 		answer.add("Xilinx");
-		System.out.println(this.projects);
-		System.out.println(answer);
 		assertEquals(answer, this.projects);
 	}
 
@@ -81,7 +92,7 @@ public class ProjectSortTest extends TestCase {
 		this.projects.add("Android Project");
 		this.projects.add("iOS Project");
 
-		Collections.sort(projects, comp);
+		Collections.sort(projects, compAZ);
 		ArrayList<String> answer = new ArrayList<String>();
 		answer.add("ABC");
 		answer.add("Android Project");
@@ -89,6 +100,52 @@ public class ProjectSortTest extends TestCase {
 		answer.add("iOS Project");
 		answer.add("Project B");
 		assertEquals(answer, this.projects);
+
+	}
+	
+	public void testSortWithNumbers() {
+		this.projects = new ArrayList<String>();
+		this.projects.add("Android Project 1");
+		this.projects.add("Bagels 1");
+		this.projects.add("Bagels 3");
+		this.projects.add("iOS project 0");
+		this.projects.add("bagels 2");
+		this.projects.add("Android Project 0");
+		this.projects.add("iOS Project 2");
+		Collections.sort(this.projects, compAZ);
 		
+		ArrayList<String> answer = new ArrayList<String>();
+		answer.add("Android Project 0");
+		answer.add("Android Project 1");
+		answer.add("Bagels 1");
+		answer.add("bagels 2");
+		answer.add("Bagels 3");
+		answer.add("iOS project 0");
+		answer.add("iOS Project 2");
+		assertEquals(answer, this.projects);
+	}
+	
+	public void testSortReverseAlphabetical() {
+		
+		this.projects = new ArrayList<String>();
+		this.projects.add("Android Project 1");
+		this.projects.add("Bagels 1");
+		this.projects.add("Bagels 3");
+		this.projects.add("iOS project 0");
+		this.projects.add("bagels 2");
+		this.projects.add("Android Project 0");
+		this.projects.add("iOS Project 2");
+		
+		Collections.sort(this.projects, compZA);
+		ArrayList<String> answer = new ArrayList<String>();
+		
+		answer.add("iOS Project 2");
+		answer.add("iOS project 0");
+		answer.add("Bagels 3");
+		answer.add("bagels 2");
+		answer.add("Bagels 1");
+		answer.add("Android Project 1");
+		answer.add("Android Project 0");
+		assertEquals(answer, this.projects);
 	}
 }
