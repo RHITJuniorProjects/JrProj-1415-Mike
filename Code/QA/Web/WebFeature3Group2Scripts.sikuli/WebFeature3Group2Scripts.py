4e425a5f810a140412af6b3b77de97397efa03c1
Zach Hull <hullzr@rose-hulman.edu>
2014-11-11 17:50:39 -0500

    testing for android and old web

--- Code/QA/Web/WebFeature3Group2Scripts.sikuli/WebFeature3Group2Scripts.py
+++ Code/QA/Web/WebFeature3Group2Scripts.sikuli/WebFeature3Group2Scripts.py
@@ -4,12 +4,12 @@ find("1413834050138.png")
 click("1413834060620.png")
 type("henry-test.firebaseapp.com/projects" + Key.ENTER);
 
-find("1413915942887.png")
-click("1413915949955.png")
-exists("1413915964434.png")
+find("1415380272421.png")
+click("1415380407005.png")
+exists("1415380185063.png")
 if exists("1413915701068.png"):
-    click("1413915708480.png")
-    if exists("1413915727080.png"):
+    click("1415380200767.png")
+    if exists("1415380232210.png"):
         print("It worked")
     else:
         print("It didn't work")
@@ -19,28 +19,28 @@ else:
  
 ## Testing the status of the project as a whole
 click("1413836181760.png")
-find("1413834175850.png")
-click("1413834184376.png")
-find("1413836030795.png")
-click("1413836047701.png")
-if exists("1413836085141.png"):
+find("1415380455013.png")
+click("1415380461436.png")
+find("1415380431586.png")
+click("1415380436857.png")
+if exists("1415380444672.png"):
     print("It worked")
 else:
     print("It didn't work");
 
 ## Testing the ability to update a task's status
 click("1413836181760.png");
-find("1413834175850.png")
-click("1413834184376.png")
-find("1413916721723.png")
-click("1413916727908.png")
-find("1415040680478.png")
-click("1415040690013.png")
-find("1413927687300.png")
-click("1415040760485.png")
+find("1415380481779.png")
+click("1415380487737.png")
+find("1415380496285.png")
+click("1415380502691.png")
+find("1415380557013.png")
+click("1415381107608.png")
+find("1415381121714.png")
+click("1415381130237.png")
 find("1413927542743.png")
 click("1413927569377.png")
-if exists("1415040403501.png"):
+if exists("1415380547528.png"):
         print("It worked")
 else:
         print("It didn't work")
