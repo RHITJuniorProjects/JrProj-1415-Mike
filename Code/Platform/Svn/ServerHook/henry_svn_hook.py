5991e961fb98331d6822d43976b97c19b26279a5
Adam Michael <ajmichael0@gmail.com>
2014-12-10 11:37:51 -0500

    prototype SVN server hook, does everything except LOC

---
+++ Code/Platform/Svn/ServerHook/henry_svn_hook.py
@@ -0,0 +1,43 @@
+from firebase import firebase
+import firebase_utils
+import operator
+import sys
+import re
+
+BASE_URL = 'https://henry-test.firebaseio.com'
+REQUIRED_FIELDS = ['email', 'hours', 'project', 'milestone', 'task', 'status']
+
+
+def commit(ref,message):
+    fields = parse(message)
+    if not reduce(operator.and_,map(lambda x: x in fields.keys(),REQUIRED_FIELDS)):
+        raise Exception('HENRY: Missing fields in commit message')
+    uID = firebase_utils.getUserID(ref,fields['email'])
+    pID = firebase_utils.getProjectID(ref,fields['project'])
+    mID = firebase_utils.getMilestoneID(ref,pID,fields['milestone'])
+    tID = firebase_utils.getTaskID(ref,pID,mID,fields['task'])
+    firebase_utils.commit(ref,fields['message'],uID,pID,mID,tID,fields['hours'],fields['status'],fields['added_lines_of_code'],fields['removed_lines_of_code'])
+
+
+def parse(message):
+    field_strings = re.findall(r'\[.*?\]',message)
+    parse_list = [map(lambda s: s.strip(),s[1:-1].lower().split(':')) for s in field_strings]
+    field_map = {v[0]:v[1] for v in parse_list}
+    bare_message = message
+    for field_string in field_strings:
+        bare_message = bare_message.replace(field_string,'')
+    base_message = bare_message.strip()
+    field_map['message'] = bare_message
+    field_map['added_lines_of_code'],field_map['removed_lines_of_code'] = loc()
+    return field_map
+
+
+def loc():
+    # not sure how to go about this
+    return 0,0
+
+
+if __name__ == '__main__':
+    ref = firebase.FirebaseApplication(BASE_URL)
+    commit_message = sys.argv[1]
+    commit(ref,commit_message)
