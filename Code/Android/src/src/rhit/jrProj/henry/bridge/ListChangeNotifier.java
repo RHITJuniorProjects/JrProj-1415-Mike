60efa59ffcd0905b8a0cd3e0c0ddc635bf65c4c2
LD <ldhamandspam@gmail.com>
2014-12-15 18:48:19 -0500

    More minor changes

--- Code/Android/src/src/rhit/jrProj/henry/bridge/ListChangeNotifier.java
+++ Code/Android/src/src/rhit/jrProj/henry/bridge/ListChangeNotifier.java
@@ -1,32 +1,9 @@
 package rhit.jrProj.henry.bridge;
 
-import android.util.Log;
-import android.widget.BaseAdapter;
+public class ListChangeNotifier {
 
-/**
- * 
- * The callback for a dynamic item in an android listview container
- *
- * @author rockwotj. Created Oct 10, 2014.
- */
-public class ListChangeNotifier<T> {
-
-	private BaseAdapter adapter;
-
-	public ListChangeNotifier(BaseAdapter adapter) {
-		this.adapter = adapter;
-	}
-	/**
-	 * 
-	 * The method that will be triggered when data is changed in the object.
-	 *
-	 */
-	public void onChange() {
-		Log.i("ListCN called", "true");
-		this.adapter.notifyDataSetChanged();
-	}
-	public BaseAdapter getAdapter(){
-		return this.adapter;
+	public ListChangeNotifier() {
+		// TODO Auto-generated constructor stub
 	}
 
 }
