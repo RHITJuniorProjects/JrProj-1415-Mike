32c18d75e863e68abf1982aabad24830a3590ca1
Zach Hull <hullzr@rose-hulman.edu>
2015-01-12 22:17:42 -0500

    android Test script

---
+++ Code/QA/Android/AndroidSprint6Feature2Script.sikuli/AndroidSprint6Feature2Script.py
@@ -0,0 +1,7 @@
+##Test Longitudinal line graph for metrics
+click("1421118888121.png")
+click("1421118844454.png")
+if exists("1421118911632.png"):
+    print("It worked")
+else:
+    print("It failed")
