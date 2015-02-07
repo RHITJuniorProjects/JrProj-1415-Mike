8ede121741473ad3239f00f5a5120e0fc31a1396
Adam <ajmichael0@gmail.com>
2015-02-07 16:35:19 -0500

    added dependencies to mac installer

--- Code/Platform/Installer/Mac/install_henry_mac.sh
+++ Code/Platform/Installer/Mac/install_henry_mac.sh
@@ -1,3 +1,11 @@
-curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-python get-pip.py --user
+apt-get install python2.7
+easy_install pip
+pip install requests
+pip install python-firebase
+wget -O /usr/bin/lib/henry https://raw.githubusercontent.com/RHITJuniorProjects/JrProj-1415-Mike/master/Code/Platform/Initializer/UnixWithSvn/henry
+wget -O /usr/bin/lib/__init__.py https://raw.githubusercontent.com/RHITJuniorProjects/JrProj-1415-Mike/master/Code/Platform/Initializer/UnixWithSvn/__init__.py
+wget -O /usr/bin/lib/firebase_utils.py https://raw.githubusercontent.com/RHITJuniorProjects/JrProj-1415-Mike/master/Code/Platform/Initializer/UnixWithSvn/firebase_utils.py
+wget -O /usr/bin/lib/git_utils.py https://raw.githubusercontent.com/RHITJuniorProjects/JrProj-1415-Mike/master/Code/Platform/Initializer/UnixWithSvn/git_utils.py
+wget -O /usr/bin/lib/svn_utils.py https://raw.githubusercontent.com/RHITJuniorProjects/JrProj-1415-Mike/master/Code/Platform/Initializer/UnixWithSvn/svn_utils.py
+ln -s /usr/bin/henry /usr/lib/henry
 
