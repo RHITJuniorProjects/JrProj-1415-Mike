cec5a0a4ad4e71a7861df3f7178f41a2f016210c
Jordan LaShomb <jtlashomb9@gmail.com>
2015-02-08 16:25:32 -0500

    leaderboard
    chris and I finished the feature

--- Code/Web/public/henry.js
+++ Code/Web/public/henry.js
@@ -443,6 +443,34 @@ User.prototype = {
         this.__firebase.off(arg1, arg2);
     }
 };
+function userLeaderboard(){
+    var pointsArray = [];
+    var arr = [];
+    console.log("here");
+    firebase.child("users").on('value', function(snapshot) {
+        var users = snapshot.val();
+        for(var id in users){
+            var name =users[id].name;
+            var points = users[id].total_points; 
+            pointsArray.push(name);
+            pointsArray.push(points);
+            arr.push(pointsArray);
+            pointsArray = [];
+        } 
+        
+    });
+    arr.sort(function(a,b) {return b[1] - a[1]});
+    console.log(arr);
+    var i =1;
+    for(var element in arr){
+        console.log(element);
+        $('#' + i).html(i);
+        $('#name' + i).html(arr[i-1][0]);
+        $('#pointValue' + i).html(arr[i-1][1]);
+        i++;
+    }
+
+};
 
 
 // Adds the currently member selected in the "Add member" modal to the project
