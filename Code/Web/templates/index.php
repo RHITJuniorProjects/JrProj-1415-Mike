8cf4a9f15beebd682eb56041a1092a75cc7fe3d7
Jordan LaShomb <jtlashomb9@gmail.com>
2015-02-15 18:46:54 -0500

    shorten home page buttons

--- Code/Web/templates/index.php
+++ Code/Web/templates/index.php
@@ -17,46 +17,48 @@
 			</div>
 			<div class="filler">
 			</div>
-			<div class="small-4 columns">
-                <button data-reveal-id="myRegisterModal" class="expand">Register</button>
-                <div id="myRegisterModal" class="reveal-modal" data-reveal>
-                    <h2>Register</h2> <br />
-                    <form action="" onsubmit="register(); return false;">
-                        <label for="registerUser">Email:</label> <input type="text" id="registerUser" />
-                        <label for="registerPass">Password:</label> <input type="password" id="registerPass" />
-                        <label for="registerPassCheck">Retype password:</label> <input type="password" id="registerPassCheck" />
-                        <label for="name">Name:</label> <input type="text" id="name"><br />
-                        <input id="registerSubmit" class="button" type="submit" value="Register" />
-                        <div id="passwordError" class="my-error" hidden>Please check that the passwords are the same.</div>
-                        <div id="registerError" class="my-error" hidden>Error registering, make sure all the fields are filled in.</div>
-                        <div id="emailError" class="my-error" hidden>Email already in use.</div>
-                    </form>
-                </div>
-			</div>
-			<div class="small-4 columns">
-				<button data-reveal-id="myLoginModal" class="expand">Login</button>
-                <div id="myLoginModal" class="reveal-modal" data-reveal>
-                	<h2>Login</h2> <br />
-                    <form action="" onsubmit="getLoginData(); return false;">
-                	    <label for="loginUser">Email:</label> <input type="text" id="loginUser" />
-                	    <label for="loginPass">Password:</label> <input type="password" id="loginPass" />
-                	    <input id="loginSubmit" class="button" type="submit" value="Login" />
-                        <div id="loginError" class="my-error" hidden>Incorrect Username or Password</div>
-                     </form>
-                </div>
-			</div>
-			<div class="small-4 columns">
-				<a href="https://github.com/RHITJuniorProjects/JrProj-1415-Mike/blob/master/Code/Platform/Installer/Windows/Henry_Installer.msi?raw=true">
-				<button class="expand">Download</button></a>
-               <!--  <div id="myAboutModal" class="reveal-modal" data-reveal>
-                    <h2>About</h2> <br />
-                    <p>
-                        Henry is a service that provides a simple way to create and manage projects and project tasks.<br>
-                        Register or login to begin!
-                    </p>
-                    <div class="button" onclick="$('#myAboutModal').foundation('reveal', 'close');">Close</div>
-                </div> -->
-			</div>
+			<div id="buttonWrapper">
+				<div class="small-4 columns">
+	                <button data-reveal-id="myRegisterModal" class="expand">Register</button>
+	                <div id="myRegisterModal" class="reveal-modal" data-reveal>
+	                    <h2>Register</h2> <br />
+	                    <form action="" onsubmit="register(); return false;">
+	                        <label for="registerUser">Email:</label> <input type="text" id="registerUser" />
+	                        <label for="registerPass">Password:</label> <input type="password" id="registerPass" />
+	                        <label for="registerPassCheck">Retype password:</label> <input type="password" id="registerPassCheck" />
+	                        <label for="name">Name:</label> <input type="text" id="name"><br />
+	                        <input id="registerSubmit" class="button" type="submit" value="Register" />
+	                        <div id="passwordError" class="my-error" hidden>Please check that the passwords are the same.</div>
+	                        <div id="registerError" class="my-error" hidden>Error registering, make sure all the fields are filled in.</div>
+	                        <div id="emailError" class="my-error" hidden>Email already in use.</div>
+	                    </form>
+	                </div>
+				</div>
+				<div class="small-4 columns">
+					<button data-reveal-id="myLoginModal" class="expand">Login</button>
+	                <div id="myLoginModal" class="reveal-modal" data-reveal>
+	                	<h2>Login</h2> <br />
+	                    <form action="" onsubmit="getLoginData(); return false;">
+	                	    <label for="loginUser">Email:</label> <input type="text" id="loginUser" />
+	                	    <label for="loginPass">Password:</label> <input type="password" id="loginPass" />
+	                	    <input id="loginSubmit" class="button" type="submit" value="Login" />
+	                        <div id="loginError" class="my-error" hidden>Incorrect Username or Password</div>
+	                     </form>
+	                </div>
+				</div>
+				<div class="small-4 columns">
+					<a href="https://github.com/RHITJuniorProjects/JrProj-1415-Mike/blob/master/Code/Platform/Installer/Windows/Henry_Installer.msi?raw=true">
+					<button class="expand">Download</button></a>
+	               <!--  <div id="myAboutModal" class="reveal-modal" data-reveal>
+	                    <h2>About</h2> <br />
+	                    <p>
+	                        Henry is a service that provides a simple way to create and manage projects and project tasks.<br>
+	                        Register or login to begin!
+	                    </p>
+	                    <div class="button" onclick="$('#myAboutModal').foundation('reveal', 'close');">Close</div>
+	                </div> -->
+				</div>
+		</div>
 		</div>
 		<div class="main-box lightblue">
 			<br/>
