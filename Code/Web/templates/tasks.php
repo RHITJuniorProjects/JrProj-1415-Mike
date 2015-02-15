1b18b8fb608d6e9e56e19732f39d6bb89578c088
hansondg <danielghanson93@gmail.com>
2015-02-15 18:23:29 -0500

    added tabs to tasks page

--- Code/Web/templates/tasks.php
+++ Code/Web/templates/tasks.php
@@ -1,37 +1,61 @@
 <div id='tasks-page' hidden>
-	<div class="main-content">
-		<table id="tasks-table" style="width:100%">
-			<thead>
-				<td>Task Name</td>
-				<td>Description</td>
-				<td>Assigned User</td> 
-				<td>Category</td>
-				<td>Status</td>
-                <td>Due Date</td>
-                <td>Is Completed</td>
-				<td>Estimated Hours</td>
-				<td class="gamification">Bounty Points</td>
-			</thead>
-			<tbody id="task-rows">
-			</tbody>
-		</table>
+	<div class="small-2 columns small-offset-1 tabbar">
+		<?php
+			require_once 'tabbar.php';
+			make_tabbar(
+				array(
+					new Tab("Back To Project",
+					        ["onclick" => "selectProject(selectedProject)"],
+							"#"),
+					new Tab("View Tasks",["class" => "active"]),
+					new Tab("Add New Task",["onclick" => "newTask()"],"#"),
+					new Tab("Task Statistics",["onclick" => "taskStatics()"],"#"),
+					new Tab("Burndown Chart")
+				)
+			);
+		?>
+	</div>
+	<div class="small-9 columns main-content">
+		<div class="tabs-content">
+			<div class="content active" id="ViewTasks">
+				<table id="tasks-table" style="width:100%">
+					<thead>
+						<td>Task Name</td>
+						<td>Description</td>
+						<td>Assigned User</td> 
+						<td>Category</td>
+						<td>Status</td>
+						<td>Due Date</td>
+						<td>Is Completed</td>
+						<td>Estimated Hours</td>
+						<td class="gamification">Bounty Points</td>
+					</thead>
+					<tbody id="task-rows">
+					</tbody>
+				</table>
+			</div>
+		<!--
 		<div class="button task-row" data-reveal-id="task-modal" onclick="newTask()">
 			Add New Task
 		</div>
 		<div class="button task-row" data-reveal-id="taskContainer" onclick="taskStatics()">
 			Task Statistics
 		</div>
-	</div>
-	<br />
-	<div id="milestone-burn-down-chart">
+		-->
+			<div class="content" id="BurndownChart">
+				<br />
+				<div id="milestone-burn-down-chart">
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 <div id="task-modal" class="reveal-modal" data-reveal>
-	<div id="taskContainer" class="reveal-modal" data-reveal>
-	</div>
 		<!-- <div class="small-12 columns wide" id="tasks-table">
 			<div class="row wide collapse" id="tasks-table-header">
 				<div class="small-1 columns" 
 			</div>
 		</div>-->
+</div>
+<div id="taskContainer" class="reveal-modal" data-reveal>
 </div>
