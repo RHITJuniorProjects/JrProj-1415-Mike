b29c2a5114804e445b169aa2a75660a77d9ffcd8
Mark Van Aken <mark.vanaken@gmail.com>
2015-03-28 18:37:23 -0400

    Made the beginnings of some of the Model classes

---
+++ Code/iOS/Henry/Henry/Classes/BountyModel.h
@@ -0,0 +1,15 @@
+//
+//  BountyModel.h
+//  Henry
+//
+//  Created by Mark Van Aken on 3/28/15.
+//  Copyright (c) 2015 Rose-Hulman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BountyModel : NSObject
+
+@property NSString* completionName;
+
+@end
